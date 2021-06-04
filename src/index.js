be5636383d73eddb58576d1d4771d002aea42a51
Pablo <pablooficial.kings@gmail.com>
2021-06-03 22:17:35 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './styles.css';
+
+import { Todo,TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(crearTodoHtml);
+
+
+ console.log('todos', todoList.todos);
+
+// const tarea = new Todo('Aprender Javascript');
+
+
+// todoList.nuevoTodo(tarea);
+
+
+// crearTodoHtml(tarea);
+
+// localStorage.setItem('mi-key','ABC123');
+// sessionStorage.setItem('mi-key','ABC123');
+// setTimeout( ()=> {
+//    localStorage.removeItem('mi-key');
+
+// },1500);
